9b828973ddb1e25c5f3e27b3e0045e5d3b512671
Dejan Koscak <de.koscak@gmail.com>
2023-07-09 18:35:14 +0200

    final corrections

--- src/Components/Messages.js
+++ src/Components/Messages.js
@@ -7,10 +7,10 @@ const Messages = ({ messages, currentMember }) => {
       : "message-position";
     return (
       <li className={className}>
-        <span className="avatar" style={{ backgroundColor: member.color }} />
+        <span className="avatar" style={{ backgroundColor: member.clientData.color }} />
         <div className="Message-content">
-          <div className="username">{member.username}</div>
-          <div className="text" style={{ borderColor: member.color }}>
+          <div className="username" style={{ color: member.clientData.color }}>{member.clientData.username}</div>
+          <div className="text" style={{ borderColor: member.clientData.color }}>
             {text}
           </div>
         </div>
