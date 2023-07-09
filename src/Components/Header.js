34126db1fba5882d5080cd965045a66b5897fdf4
Dejan Koscak <de.koscak@gmail.com>
2023-07-09 19:02:45 +0200

    final version

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -10,13 +10,13 @@ export default class Header extends React.Component {
         <li>CHAT APP</li>
         <li id="bubble">💬</li>
         <li>CHAT APP</li>
-        <li id="bubble">👻</li>
+        <li id="bubble">💬</li>
       </ul>
       <ul class="marquee__content" aria-hidden="true">
         <li>CHAT APP</li>
         <li id="bubble">💬</li>
         <li>CHAT APP</li>
-        <li id="bubble">👻</li>
+        <li id="bubble">💬</li>
       </ul>
     </div>
       </div>
